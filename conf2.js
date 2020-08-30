279f7813c02c1205fb0e9fe20c5cc8df18002c48
mahesh1049 <67138400+mahesh1049@users.noreply.github.com>
2020-08-30 19:43:21 +0530

    Create conf2.js

---
+++ conf2.js
@@ -0,0 +1,24 @@
+//protractor firefoxconfig.js
+exports.config = {
+  framework: 'jasmine',
+  directConnect: false, //Start protractor without start the selenium server using webdriver-manager start. default value is fales
+  //This is  only for chrome and firefox and use drivers instead of selenium server
+     
+  capabilities: {
+   browserName: 'firefox',
+   'moz:firefoxOptions': {
+         args: ['--verbose'],
+    //     binary: 'C:/Program Files/Mozilla Firefox/firefox.exe' //Provide binary location to avoid potential binary not found errors 
+    //Need to start cmd via admin mode to avoid permission error
+     }
+},   
+ //set to true So each spec will be executed in own browser instance. default value is false
+ //restartBrowserBetweenTests: true,
+  jasmineNodeOpts: {
+ //Jasmine provides only one timeout option  timeout in milliseconds don't add ;
+ defaultTimeoutInterval: 180000
+  },
+ 
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['src/com/sam/scriptjs/iframes.spec.js']
+}
