434f3a168a1a098ef5e34b7e37d8f53593328617
mahesh1049 <67138400+mahesh1049@users.noreply.github.com>
2020-08-30 19:41:32 +0530

    Update conf1.js

--- conf1.js
+++ conf1.js
@@ -1,11 +1,11 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
-        browserName: 'firefox',
+        browserName: 'chrome',
 
-//         chromeOptions: {
-//             args: [ "--headless", "--disable-gpu", "--no-sandbox", "--window-size=800,600" ]
-//         }
+        chromeOptions: {
+            args: [ "--headless", "--disable-gpu", "--no-sandbox", "--window-size=800,600" ]
+        }
     },
   specs: ['todo-spec.js']
 };
